d2f08f4f0d951b4cf19f02e651787db0df1873d1
Minh Ngo <ngo.minh0224@gmail.com>
2021-02-10 06:07:22 -0800

    header

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CoolHeader extends Component {
+    render() {
+        return (
+            <header>
+               <p className="header-text">Come take a look!</p>
+            </header>
+        )
+    }
+}
